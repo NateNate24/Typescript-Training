ac8a923f2258d614bd63ea3e35e7918c65420f25
Nathan.Bandilla <60598430+NateNate24@users.noreply.github.com>
2020-05-13 10:54:05 +0800

    In depth typescript session

---
+++ LIBRARYMANAGER/app.ts
@@ -0,0 +1,8 @@
+class HellowWorld {
+    constructor(public message: string) {}
+}
+
+var hello = new HellowWorld('Hello 1 Typescript');
+console.log(hello.message);
+
+console.log('Welcome VS Code');
---
+++ LIBRARYMANAGER/js/app.js
@@ -0,0 +1,11 @@
+"use strict";
+var HellowWorld = /** @class */ (function () {
+    function HellowWorld(message) {
+        this.message = message;
+    }
+    return HellowWorld;
+}());
+var hello = new HellowWorld('Hello 1 Typescript');
+console.log(hello.message);
+console.log('Welcome VS Code');
+//# sourceMappingURL=app.js.map
